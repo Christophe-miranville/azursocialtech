d5d1b70295781b096b73d5b61cb187b573aae2c0
christophe-miranville <christophe.miranville@laposte.net>
2024-10-21 02:29:10 +0200

    add_feature_react_toggle

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const elements = [document.getElementById("like"),
-document.getElementById("love"),
-document.getElementById("dislike")]
-
-elements.forEach((element) => {
-    element.addEventListener("click", () => {
-        const parent = element.parentElement;
-
-        [...parent.children].forEach((parentChildrenElement) => {
-            if (parentChildrenElement !== element) {
-                parentChildrenElement.classList.remove("fa-solid");
-                parentChildrenElement.classList.add("fa-regular");
-            }
+    document.getElementById("love"),
+    document.getElementById("dislike")]
+    
+    elements.forEach( (element) => {
+        element.addEventListener("click", () => {
+            const parent = element.parentElement;
+    
+            [...parent.children].forEach( (parentChildrenElement) => {
+               if(parentChildrenElement !== element)
+                {
+                  parentChildrenElement.classList.add("fa-regular");
+                  parentChildrenElement.classList.remove("fa-solid");
+               }
+           });
+    
+           element.classList.toggle("fa-regular");
+           element.classList.toggle("fa-solid");
         });
-
-        element.classList.toggle("fa-regular");
-        element.classList.toggle("fa-solid");
     });
-});
