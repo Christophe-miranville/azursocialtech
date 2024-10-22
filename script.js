d8fa427e20867b5a7ab7e6ee213f940af197149e
christophe-miranville <christophe.miranville@laposte.net>
2024-10-23 00:02:02 +0200

    comment_js_reactions

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+//Gestion des reactions de post
 const elements = [document.getElementById("like"),
-    document.getElementById("love"),
-    document.getElementById("dislike")]
-    
-    elements.forEach( (element) => {
-        element.addEventListener("click", () => {
-            const parent = element.parentElement;
-    
-            [...parent.children].forEach( (parentChildrenElement) => {
-               if(parentChildrenElement !== element)
-                {
-                  parentChildrenElement.classList.add("fa-regular");
-                  parentChildrenElement.classList.remove("fa-solid");
-               }
-           });
-    
-           element.classList.toggle("fa-regular");
-           element.classList.toggle("fa-solid");
+document.getElementById("love"),
+document.getElementById("dislike")]
+
+// Boucle forEach afin d'appliquer l'écouteur d'événement sur chaque element reaction
+elements.forEach((element) => {
+    element.addEventListener("click", () => {
+        const parent = element.parentElement;
+        // Boucle forEach en ciblant tous les enfants du parent element
+        [...parent.children].forEach((parentChildrenElement) => {
+            //Vérification que ce n'est pas l'élément cliqué afin de réinitialiser son état
+            if (parentChildrenElement !== element) {
+                //On enlève la classe fa-solid pour désactiver les autres réactions
+                parentChildrenElement.classList.remove("fa-solid");
+                //On réattribue la classe fa-regular pour remettre les autres boutons dans leur état initial
+                parentChildrenElement.classList.add("fa-regular");
+            }
         });
+        //Mise en place d'un toggle afin de basculer entres les 2 classes pour l'élément cliqué : activation ou désactivation de la réaction en alternant les classes
+        element.classList.toggle("fa-regular");
+        element.classList.toggle("fa-solid");
     });
+});
