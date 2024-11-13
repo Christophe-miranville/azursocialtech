9801be577eb00252c36f3a930e05ad33695a212d
christophe-miranville <christophe.miranville@laposte.net>
2024-11-13 22:06:32 +0100

    edit_css_messenger_and_use_json

--- js/messenger.js
+++ js/messenger.js
@@ -3,14 +3,15 @@ async function loadMessengerData() {
         const res = await fetch("json/messenger.json");
         const data = await res.json();
 
-        messengerView(data.discussions);
+        discussionView(data.discussions);
+        messengerDetails(data.discussions);
     } catch (error) {
         console.error("Erreur lors du chargement", error);
     }
 }
 
-function messengerView(discussions) {
-    const messengerContainer = document.getElementById("discussions-container");
+function discussionView(discussions) {
+    const discussionContainer = document.getElementById("discussions-container");
 
     discussions.forEach(discussion => {
         const discussionElement = document.createElement("div");
@@ -24,8 +25,31 @@ function messengerView(discussions) {
                     </div>
                     <button><a href="messenger.html">more...</a></button>
         `;
-        messengerContainer.appendChild(discussionElement);
+        discussionContainer.appendChild(discussionElement);
     });
+
+}
+
+function messengerDetails(discussions) {
+    const messengerContainer = document.getElementById("messenger-container");
+
+    discussions.forEach(discussion => {
+        const messengerElement = document.createElement("div");
+        messengerElement.classList.add("messages");
+
+        messengerElement.innerHTML =
+            ` <div class="profil_messenger">
+                        <img src="${discussion.picture}" alt="discussion_picture">
+                        <p class="name_messenger">${discussion.pseudo}</p>
+                    </div>
+                    <div class="message">
+                        <p class="name_contact">Kriss</p>
+                        <p class="last_message">${discussion.last_message}</p>
+                        <p date>01/01/2024 12:00</p>
+                    </div>
+                    `;
+        messengerContainer.appendChild(messengerElement);
+    })
 
 }
 
